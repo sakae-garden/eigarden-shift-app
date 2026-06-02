fb66cacfd23229ad8c8f1912c566dc3c77aab509
sugi <sugi.forwork@gmail.com>
2026-06-02 19:32:19 +0900

    feat: PWA対応（ホーム画面追加）と時間変更機能を追加

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'eigarden-shift-v1';
+const SHELL = ['./', './index.html', './app-config.js', './manifest.json', './icon-512.png'];
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(()=>{})); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
+self.addEventListener('fetch', e => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  const url = new URL(req.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      fetch(req).then(res => { const copy = res.clone(); caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{}); return res; })
+        .catch(() => caches.match(req).then(r => r || caches.match('./index.html')))
+    );
+  }
+});
