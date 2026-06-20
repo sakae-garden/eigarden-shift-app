3b27f6e938ab8fb43acc115f4255199a4e0e2752
sugi <sugi.forwork@gmail.com>
2026-06-20 12:20:58 +0900

    fix: 印刷をA4横・用紙幅いっぱい・文字拡大に改善

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'eigarden-shift-v6';
+const CACHE = 'eigarden-shift-v7';
 const SHELL = ['./', './index.html', './app-config.js', './manifest.json', './icon-512.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).catch(()=>{})); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())); });
